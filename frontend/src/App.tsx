4165d21a3eaadbbe44affc3196a167258bf660a4
andiemasson <aclegg1@byu.edu>
2025-03-28 13:08:18 -0600

    Phase 3 commmit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import CategoryFilter from './CategoryFilter';
 import ProjectList from './ProjectList'
 
 function App() {
 
   return (
     <>
+      <CategoryFilter/>
       <ProjectList/>
+      
     </>
   )
 }
